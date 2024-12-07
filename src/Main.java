20480d1551965a9b57a405747e9bdd5a3bace04e
Lavsrl <lavsrochabdo@gmail.com>
2024-12-07 20:28:07 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,7 @@ public class Main {
                 gerenciador.desalocar(processo);
             } else {
                 if (!algoritmo.apply(processo)) {
-                    System.out.println(" Não foi possível alocar o " + processo.getId());
+                    System.out.println("Não foi possível alocar o " + processo.getId());
                 }
             }
             gerenciador.exibirMemoria();
